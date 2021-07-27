614b1a9e95f376b7c1e0ac0743c1d72d320e1e3f
Stackbit Code Editor <projects@stackbit.com>
2021-07-27 07:19:02 +0000

    ContactUs.js: changed

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class ContactSection extends React.Component {
+export default class ContactU extends React.Component {
     render() {
         return ( 
         );
