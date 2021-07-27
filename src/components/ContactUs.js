d8ed468583b69548bebc0036271921573a1da92d
Stackbit Code Editor <projects@stackbit.com>
2021-07-27 07:31:42 +0000

    ContactUs.js: changed

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 import {classNames, htmlToReact, withPrefix, Link, markdownify, toStyleObj} from '../utils';
 
-export default class Detail extends React.Component {
+export default class DetailUs extends React.Component {
     render() {
         console.log(this.props );
         let section = _.get(this.props, 'section', null);
